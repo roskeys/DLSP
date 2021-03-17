791b409e7132ef3ef9546cf04debc3abfbaeaed0
roskey <lsjsg@outlook.com>
2021-03-17 13:13:15 +0800

    Fix cuda error and remove print

--- model.py
+++ model.py
@@ -32,26 +32,21 @@ class BottleNeck(nn.Module):
 
     def forward(self, model):
         identity = model
-        print("New BottleNeck Block.", "The input shape is:", model.shape)
 
         out = self.conv1(model)
         out = self.bn1(out)
         out = self.relu(out)
-        print("First Conv Layer output shape is:", out.shape)
 
         out = self.conv2(out)
         out = self.bn2(out)
         out = self.relu(out)
-        print("Second Conv Layer output shape is:", out.shape)
 
         out = self.conv3(out)
         out = self.bn3(out)
-        print("Third Conv Layer output shape is:", out.shape)
 
         if self.shortcut:
             identity = self.convs(model)
             identity = self.bns(identity)
-            print("Shortcut Conv Layer output shape is:", identity.shape)
 
         out += identity
         out = self.relu(out)
@@ -70,23 +65,24 @@ class BottleNeckLayer(nn.Module):
         """
         super(BottleNeckLayer, self).__init__()
         self.expansion = 4
-        self.blocks = []
         if downsample:
             self.bottleneck1 = BottleNeck(in_channels, out_channels, stride=2, shortcut=True)
         else:
             self.bottleneck1 = BottleNeck(in_channels, out_channels, stride=1, shortcut=True)
+
+        blocks = []
         for index in range(modules - 1):
-            self.blocks.append(BottleNeck(out_channels * self.expansion, out_channels, stride=1, shortcut=False))
+            blocks.append(BottleNeck(out_channels * self.expansion, out_channels, stride=1, shortcut=False))
+        self.block = nn.Sequential(*blocks)
 
     def forward(self, model):
         out = self.bottleneck1(model)
-        for block_index in range(len(self.blocks)):
-            out = self.blocks[block_index](out)
+        out = self.block(out)
         return out
 
 
 class Resnet50(nn.Module):
-    def __init__(self, hidden_dim=1024, out_dim=2, dropout_p=0.5, device=None):
+    def __init__(self, hidden_dim=1024, out_dim=2, dropout_p=0.5):
         super(Resnet50, self).__init__()
         self.conv = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=7, padding=3, stride=2, bias=False)
         self.bn = nn.BatchNorm2d(64)
@@ -121,98 +117,6 @@ class Resnet50(nn.Module):
 
 if __name__ == '__main__':
     # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    x = torch.randn((32, 1, 150, 150))
-    model = Resnet50()
+    x = torch.randn((32, 1, 150, 150)).cuda()
+    model = Resnet50().cuda()
     out = model(x)
-# class Bottleneck(nn.Module):
-#     """
-#     in_channels: input channels
-#     out_channels: output channels for second Conv2d layer.
-#         The true output channel fed to the next module will have out_channels * expansion channels
-#     stride: stride number
-#     down_sampling: if set to true, the first module will be set to use downsampling, causing stride = 2
-#     expansion: expansion rate of channels
-#     """
-#
-#     def __init__(self, in_channels, out_channels, stride=1, down_sampling=False, expansion=4):
-#         super(Bottleneck, self).__init__()
-#         self.expansion = expansion
-#         self.down_sampling = down_sampling
-#         self.bottle_neck = nn.Sequential(
-#             nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=1, bias=False),
-#             nn.BatchNorm2d(out_channels),
-#             nn.ReLU(inplace=True),
-#             nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, stride=stride, padding=1,
-#                       bias=False),
-#             nn.BatchNorm2d(out_channels),
-#             nn.ReLU(inplace=True),
-#             nn.Conv2d(in_channels=out_channels, out_channels=out_channels * self.expansion, kernel_size=1, stride=1,
-#                       bias=False),
-#             nn.BatchNorm2d(out_channels * self.expansion)
-#         )
-#         if self.down_sampling:
-#             self.down_sampling = nn.Sequential(
-#                 nn.Conv2d(in_channels=in_channels, out_channels=out_channels * self.expansion, kernel_size=1,
-#                           stride=stride, bias=False),
-#                 nn.BatchNorm2d(out_channels * self.expansion)
-#             )
-#         self.relu = nn.ReLU(inplace=True)
-#
-#     def forward(self, x):
-#         residual = x
-#         out = self.bottle_neck(x)
-#         if self.down_sampling:
-#             residual = self.down_sampling(x)
-#         out += residual
-#         out = self.relu(out)
-#         return out
-#
-#
-# class ResNet(nn.Module):
-#     """
-#     Resnet class
-#     blocks: Number of blocks for each bottle neck
-#     num_classes: Output categories
-#     expansion: expansion rate of channels
-#     """
-#
-#     def __init__(self, blocks, num_classes=1, expansion=4):
-#         super(ResNet, self).__init__()
-#         self.expansion = expansion
-#         self.conv = nn.Sequential(
-#             nn.Conv2d(in_channels=1, out_channels=64, kernel_size=7, stride=2, padding=3,
-#                       bias=False),
-#             nn.BatchNorm2d(64),
-#             nn.ReLU(inplace=True),
-#             nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-#         )
-#         self.bottle_neck_1 = self.make_layer(in_channels=64, out_channels=64, block=blocks[0], stride=1)
-#         self.bottle_neck_2 = self.make_layer(in_channels=256, out_channels=128, block=blocks[1], stride=2)
-#         self.bottle_neck_3 = self.make_layer(in_channels=512, out_channels=256, block=blocks[2], stride=2)
-#         self.bottle_neck_4 = self.make_layer(in_channels=1024, out_channels=512, block=blocks[3], stride=2)
-#         self.avgpool = nn.AvgPool2d(kernel_size=5, stride=1)
-#         self.fc = nn.Linear(2048, num_classes)
-#
-#     def make_layer(self, in_channels, out_channels, block, stride):
-#         layers = [Bottleneck(in_channels=in_channels, out_channels=out_channels, stride=stride, down_sampling=True)]
-#         for i in range(1, block):
-#             layers.append(Bottleneck(in_channels=out_channels * self.expansion, out_channels=out_channels))
-#         return nn.Sequential(*layers)
-#
-#     def forward(self, image):
-#         x = self.conv(image)
-#         x = self.bottle_neck_1(x)
-#         x = self.bottle_neck_2(x)
-#         x = self.bottle_neck_3(x)
-#         x = self.bottle_neck_4(x)
-#         x = self.avgpool(x)
-#         x = x.view(x.size(0), -1)
-#         x = self.fc(x)
-#         return x
-#
-#
-# if __name__ == '__main__':
-#     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-#     x = torch.randn((32, 1, 150, 150)).cuda()
-#     model = ResNet([3, 4, 6, 3]).cuda()
-#     out = model(x)
