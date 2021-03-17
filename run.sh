2d1116800a4cbac0580217c2013d3c404f73ca31
roskey <lsjsg@outlook.com>
2021-03-17 23:11:12 +0800

    Added running function

---
+++ run.sh
@@ -0,0 +1,8 @@
+## train a classifier for 3 different classes
+python --model 1 --epochs=200 --print_every 1 --save_every 10
+
+## train a classifier for infected
+python --model 2 --epochs=200 --print_every 1 --save_every 10
+
+## train a classifier for covid
+python --model 3 --epochs=200 --print_every 1 --save_every 10
